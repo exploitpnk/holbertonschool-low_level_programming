79ae9473077330ba5093157b0aea1a4fc49c817c
exploitpnk <1245@holbertonschool.com>
2020-04-27 19:26:13 -0500

    [Add] init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include "dog.h"
 /**
-* init_dog - init structure
-* @d: pointer to structure
-* @name: name of dog
-* @age: age of dog
-* @owner: owner of dog
-**/
+ * init_dog - init
+ * @d: ptr
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Return: 1
+ **/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
 }
