cd375e119ac98cfdcd404fa45980517ef312fc63
exploitpnk <exploitpnk@gmail.com>
2020-05-01 03:09:53 -0500

    [Add] change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints minimum number of coins to make change for an amount of money.
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ **/
+int main(int argc, char **argv)
+{
+	int initialamount, i;
+	int cents = 0;
+	int cent_coins[] = {25, 10, 5, 2, 1};
+
+	if (argc == 2)
+	{
+		initialamount = atoi(argv[1]);
+		for (i = 0; initialamount != 0; i++)
+		{
+			cents = (cents + initialamount / cent_coins[i]);
+			initialamount = (initialamount % cent_coins[i]);
+		}
+		printf("%d\n", cents);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
