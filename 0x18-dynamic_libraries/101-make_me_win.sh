d1e1ffd8cc88533b99459f36b7dae9ea8cbb55ed
exploitpnk <exploitpnk@gmail.com>
2020-05-04 21:48:44 -0500

    [Fix] output

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/exploitpnk/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -o /tmp/inject.so
+wget -q0 /tmp/inject.so https://github.com/exploitpnk/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so
 export LD_PRELOAD=/tmp/inject.so
